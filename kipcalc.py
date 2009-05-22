85ced50c25849ec0ef933f793d5dd9a78a93e39f
Kyle VanderBeek <kylev@kylev.com>
2009-05-22 16:01:53 -0700

    first bumbling attempts to draw the UI in python

--- kipcalc.py
+++ kipcalc.py
@@ -2,16 +2,38 @@
 
 import sys
 
-from PyKDE4 import kdecore # import ki18n, KAboutData, KCmdLineArgs
-from PyKDE4 import kdeui # import KApplication
+from PyKDE4 import kdecore
+from PyKDE4 import kdeui
 
-from PyQt4.QtGui import QLabel
+from PyQt4.QtGui import QLabel, QGroupBox
 
 
-class MainWindow(kdeui.KMainWindow):
+class IPWidget(kdeui.KVBox):
+    def __init__(self, parent=None):
+        super(IPWidget, self).__init__(parent)
+
+        host_box = QGroupBox("Host", self)
+        QLabel('Dot', host_box)
+        dot_entry = kdeui.KRestrictedLine(host_box)
+        dot_entry.setValidChars('1234567890.')
+
+        nm_box = QGroupBox("NetMask", self)
+
+
+class Kipcalc(kdeui.KMainWindow):
     def __init__ (self):
-        super(MainWindow, self).__init__()
+        super(Kipcalc, self).__init__()
+        #kdeui.KMainWindow.__init__(self)
+        self.help = QLabel(kdecore.i18n("Some Help"), self)
 
+        menu = self.menuBar()
+
+        f_menu = kdeui.KMenu("File", menu)
+        f_menu.addAction("Quit")
+
+        self.setCentralWidget(IPWidget(self))
+
+    def blah(self):
         self.resize(640, 480)
         label = QLabel("This is a simple PyKDE4 program", self)
         label.setGeometry (10, 10, 200, 20)
@@ -35,6 +57,6 @@ if '__main__' == __name__:
     kdecore.KCmdLineArgs.init(sys.argv, aboutData)
     app = kdeui.KApplication()
 
-    mainWindow = MainWindow()
+    mainWindow = Kipcalc()
     mainWindow.show()
     app.exec_()
